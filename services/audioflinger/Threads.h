9e4535a6413f8d5a8e5a474a730bf60d14f9c00c
syphyr <syphyr@gmail.com>
2017-03-26 01:56:30 +0100

    audioflinger: increase max tracks per uid
    Fix some apps from not being able to initialize audio tracks
    and prevent the following error, "AudioFlinger could not create
    track. status: -12".
    
    Change-Id: I37c6a086e4a2779d3bdac64f716f2167bfe8068f

--- services/audioflinger/Threads.h
+++ services/audioflinger/Threads.h
@@ -510,8 +510,8 @@ public:
     static const int8_t kMaxTrackRetriesOffload = 20;
     static const int8_t kMaxTrackStartupRetriesOffload = 100;
     static const int8_t kMaxTrackStopRetriesOffload = 2;
-    // 16 tracks max per client allows for 2 misbehaving application leaving 6 available tracks.
-    static const uint32_t kMaxTracksPerUid = 16;
+    // 18 tracks max per client allows for 2 misbehaving application leaving 8 available tracks.
+    static const uint32_t kMaxTracksPerUid = 18;
 
     PlaybackThread(const sp<AudioFlinger>& audioFlinger, AudioStreamOut* output,
                    audio_io_handle_t id, audio_devices_t device, type_t type, bool systemReady);
